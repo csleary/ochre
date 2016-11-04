7c3923f779bca67f66ebb1b974258598358a8aa4
Chris <Chris@Christophers-MacBook-Pro.local>
2016-11-04 15:36:50 +0000

    Tweaked nav menu, sequenced scripts to defer contact ajax.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-// Parallax
 $(function() {
 	$.stellar({
 		horizontalScrolling: false,
 	});
 });
-
-// Responsive nav menu
 
 function nav() {
     var x = document.getElementById("main-nav");
@@ -15,41 +12,3 @@ function nav() {
         x.className = "nav-menu";
     }
 }
-
-// Contact form AJAX
-var $contactForm = $('#contact-form');
-
-$contactForm.submit(function(e) {
-	e.preventDefault();
-	var $submit = $('input:submit', $contactForm);
-	var defaultSubmitText = $submit.val();
-
-	$.ajax({
-		url: '//formspree.io/mail@ochremusic.com',
-		method: 'POST',
-		data: $(this).serialize(),
-		dataType: 'json',
-		beforeSend: function() {
-			//$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-			$submit.attr('disabled', true).val('Sending message…');
-		},
-		success: function(data) {
-			//$contactForm.append('<div class="alert alert--success">Message sent!</div>');
-			$submit.val('Message sent!');
-      $("#contact-form")[0].reset();
-			setTimeout(function() {
-				//$('.alert--success').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
-		},
-		error: function(err) {
-			//$contactForm.find('.alert--loading').hide();
-			//$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-			$submit.val('Ops, there was an error.');
-			setTimeout(function() {
-				//$('.alert--error').remove();
-				$submit.attr('disabled', false).val(defaultSubmitText);
-			}, 5000);
-		}
-	});
-});
