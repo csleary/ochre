53a85b229811c0ca95eee298bda9c7d6a9a5bfe0
Chris <Chris@Christophers-MacBook-Pro.local>
2016-11-11 12:25:22 +0000

    Moved nav script for quicker load on mobile devices.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,6 @@ $.stellar({
   horizontalScrolling: false
 });
 
-function nav() {
-  var x = document.getElementById("main-nav");
-  if (x.className === "nav-menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "nav-menu";
-  }
-}
-
 var $contactForm = $("#contact-form");
 
 $contactForm.submit(function(e) {
