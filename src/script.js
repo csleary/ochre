fc8ad97020a4f1c0ed2ff5a453ac9bea7b866cad
Chris <csleary@gmail.com>
2019-04-17 15:11:17 +0200

    New post, update bio, sidebar, nav.

--- src/script.js
+++ src/script.js
@@ -22,57 +22,6 @@ $.get("https://ochremusic.com/api/mailchimp", function(data) {
   $("aside #mailchimp").html(data.mailchimp);
 });
 
-// var socket = new WebSocket('wss://ochremusic.com/followers/');
-// var socket = new WebSocket("ws://localhost:8081");
-// socket.onmessage = function(response) {
-//   var data = JSON.parse(response.data);
-//   switch (data.service) {
-//     case 'spotify':
-//       $('#followers .spotify').html(data.followers);
-//       break;
-//     case 'soundcloud':
-//       $('#followers .soundcloud').html(data.followers);
-//       break;
-//     case 'facebook':
-//       $('#followers .facebook').html(data.likes);
-//       break;
-//     case 'twitter':
-//       $('#followers .twitter').html(data.followers);
-//       break;
-//     case 'mailchimp':
-//       $('aside #mailchimp').html(data.subscribers);
-//       break;
-//     case 'songkick':
-//       songkickList = data.gigs;
-//
-//       function songkick(gig) {
-//         var list = document.createElement('ul');
-//         for (i = 0; i < gig.length; i++) {
-//           var item = document.createElement('li');
-//           item.appendChild(document.createTextNode(gig[i].date));
-//           item.appendChild(document.createTextNode('\u00A0' + String.fromCharCode(8226) + '\u00A0'));
-//           var a = document.createElement('a');
-//           var linkText = document.createTextNode(gig[i].venue);
-//           a.appendChild(linkText);
-//           a.title = "Visit the event page on Songkick.";
-//           a.href = gig[i].link;
-//           item.appendChild(a);
-//           item.appendChild(document.createElement('br'));
-//           item.appendChild(document.createTextNode(gig[i].location));
-//           item.appendChild(document.createTextNode(gig[i].time));
-//           list.appendChild(item);
-//         }
-//         return list;
-//       }
-//
-//       if (songkickList) {
-//         var placeholder = document.getElementById('sk-none');
-//         document.getElementById('songkick').replaceChild(songkick(songkickList), placeholder);
-//       }
-//       break;
-//   }
-// };
-
 $("#eth-button").click(function() {
   $(".eth-info").toggle();
   $(".btc-info").hide();
@@ -103,24 +52,18 @@ $contactForm.submit(function(e) {
     data: $(this).serialize(),
     dataType: "json",
     beforeSend: function() {
-      //$contactForm.append("<div class="alert alert--loading">Sending message…</div>");
       $submit.attr("disabled", true).val("Sending message…");
     },
     success: function(data) {
-      //$contactForm.append("<div class="alert alert--success">Message sent!</div>");
       $submit.val("Message sent!");
       $("#contact-form")[0].reset();
       setTimeout(function() {
-        //$(".alert--success").remove();
         $submit.attr("disabled", false).val(defaultSubmitText);
       }, 5000);
     },
     error: function(err) {
-      //$contactForm.find(".alert--loading").hide();
-      //$contactForm.append("<div class="alert alert--error">Error.</div>");
       $submit.val("Sending failed, sorry.");
       setTimeout(function() {
-        //$(".alert--error").remove();
         $submit.attr("disabled", false).val(defaultSubmitText);
       }, 5000);
     }
