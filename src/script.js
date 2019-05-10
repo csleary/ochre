8efd23d00e069851db05b20dc28e44a1f8facccc
Chris <csleary@gmail.com>
2019-05-10 16:40:47 +0200

    Add img lazyload.

--- src/script.js
+++ src/script.js
@@ -1,84 +1,99 @@
 $.stellar({
-  horizontalScrolling: false
+  horizontalScrolling: false,
 });
 
-$.get("https://ochremusic.com/api/spotify", function(data) {
-  $("#followers .spotify").html(data.spotify);
+document.addEventListener('lazybeforeunveil', function(e) {
+  var bg = e.target.getAttribute('data-bg');
+  if (bg) {
+    e.target.style.backgroundImage = 'url(' + bg + ')';
+  }
 });
 
-$.get("https://ochremusic.com/api/soundcloud", function(data) {
-  $("#followers .soundcloud").html(data.soundcloud);
+$.get('https://ochremusic.com/api/spotify', function(data) {
+  $('#followers .spotify').html(data.spotify);
 });
 
-$.get("https://ochremusic.com/api/facebook", function(data) {
-  $("#followers .facebook").html(data.facebook);
+$.get('https://ochremusic.com/api/soundcloud', function(data) {
+  $('#followers .soundcloud').html(data.soundcloud);
 });
 
-$.get("https://ochremusic.com/api/twitter", function(data) {
-  $("#followers .twitter").html(data.twitter);
+$.get('https://ochremusic.com/api/facebook', function(data) {
+  $('#followers .facebook').html(data.facebook);
 });
 
-$.get("https://ochremusic.com/api/mailchimp", function(data) {
-  $("aside #mailchimp").html(data.mailchimp);
+$.get('https://ochremusic.com/api/twitter', function(data) {
+  $('#followers .twitter').html(data.twitter);
 });
 
-$("#eth-button").click(function() {
-  $(".eth-info").toggle();
-  $(".btc-info").hide();
-  $(".xem-info").hide();
+$.get('https://ochremusic.com/api/mailchimp', function(data) {
+  $('aside #mailchimp').html(data.mailchimp);
 });
 
-$("#xem-button").click(function() {
-  $(".xem-info").toggle();
-  $(".btc-info").hide();
-  $(".eth-info").hide();
-});
-
-$("#btc-button").click(function() {
-  $(".btc-info").toggle();
-  $(".eth-info").hide();
-  $(".xem-info").hide();
-});
-
-var $contactForm = $("#contact-form");
-$contactForm.submit(function(e) {
-  e.preventDefault();
-  var $submit = $("input:submit", $contactForm);
+$('#list-subscribe').submit(function(event) {
+  event.preventDefault();
+  var $submit = $('input:submit', $(this));
   var defaultSubmitText = $submit.val();
 
-  $.ajax({
-    url: "//formspree.io/" + "mail" + "@" + "ochremusic" + "." + "com",
-    method: "POST",
-    data: $(this).serialize(),
-    dataType: "json",
-    beforeSend: function() {
-      $submit.attr("disabled", true).val("Sending message…");
-    },
-    success: function(data) {
-      $submit.val("Message sent!");
-      $("#contact-form")[0].reset();
-      setTimeout(function() {
-        $submit.attr("disabled", false).val(defaultSubmitText);
-      }, 5000);
-    },
-    error: function(err) {
-      $submit.val("Sending failed, sorry.");
-      setTimeout(function() {
-        $submit.attr("disabled", false).val(defaultSubmitText);
-      }, 5000);
+  $submit.prop('disabled', true).val('Sending…');
+
+  var url = $(this).attr('action');
+  var data = $(this).serialize();
+
+  $.post(url, data, function(res) {
+    $submit.prop('disabled', false).val(defaultSubmitText);
+    if (res.error && res.status === 'subscribed') {
+      $('#list-res-error').html('Error: ' + res.error);
+      $('#list-res').html(
+        'You can update your profile <a href="' + res.updateUrl + '">here</a>.'
+      );
+      return;
     }
+    if (
+      res.error &&
+      (res.status === 'unsubscribed' || res.status === 'pending')
+    ) {
+      $('#list-res-error').html('Error: ' + res.error);
+      $('#list-res').html(
+        'You can always resubscribe <a href="http://eepurl.com/gphAPb">here</a>. You will receive a confirmation opt-in email.'
+      );
+      return;
+    }
+    if (res.error) {
+      $('#list-res-error').html('Error: ' + res.error);
+      $('#list-res').empty();
+      return;
+    }
+    $('#list-res-error').empty();
+    $('#list-res').html(res.success);
   });
 });
 
-$(window).load(function() {
-  setTimeout(function() {
-    $('svg[data-hash="5652f831c72d"]').each(function(i) {
-      $(this)
-        .parent()
-        .next("div.text-wrapper")
-        .find("a.author")
-        .first()
-        .addClass("chris");
-    });
-  }, 500);
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+  e.preventDefault();
+  var $submit = $('input:submit', $contactForm);
+  var defaultSubmitText = $submit.val();
+
+  $.ajax({
+    url: '//formspree.io/' + 'mail' + '@' + 'ochremusic' + '.' + 'com',
+    method: 'POST',
+    data: $(this).serialize(),
+    dataType: 'json',
+    beforeSend: function() {
+      $submit.attr('disabled', true).val('Sending message…');
+    },
+    success: function(data) {
+      $submit.val('Message sent!');
+      $('#contact-form')[0].reset();
+      setTimeout(function() {
+        $submit.attr('disabled', false).val(defaultSubmitText);
+      }, 5000);
+    },
+    error: function(err) {
+      $submit.val('Sending failed, sorry.');
+      setTimeout(function() {
+        $submit.attr('disabled', false).val(defaultSubmitText);
+      }, 5000);
+    },
+  });
 });
