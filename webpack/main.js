ed1de2813cd1e8ebd42aa63633e547be13d7fd28
Chris Leary <csleary@gmail.com>
2024-11-09 13:47:43 +0100

    Remove old social links.

--- webpack/main.js
+++ webpack/main.js
@@ -23,12 +23,6 @@ $(document).ready(() => {
       nav.removeClass('sticky');
     }
   });
-
-  // $.get('/.netlify/functions/socials', (data) => {
-  //   const { spotify, twitter } = JSON.parse(data);
-  //   $('#followers .spotify').html(spotify);
-  //   $('#followers .twitter').html(twitter);
-  // });
 });
 
 const contactForm = $('#contact-form');
