76700df786a0758fed9de949838c0268cd0b3ff1
Chris Leary <csleary@gmail.com>
2024-12-02 21:42:27 +0100

    Remove jquery.

--- webpack/main.js
+++ webpack/main.js
@@ -1,68 +1,24 @@
-$(document).ready(() => {
-  const container = $('.container');
-  let containerTop;
-  const header = $('header.parallax');
-  let headerPosition;
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.container');
+  const header = document.querySelector('header.parallax');
   let lastScrollY = 0;
-  const nav = $('nav');
+  let ticking = false;
 
-  $(window).scroll(() => {
-    lastScrollY = $(window).scrollTop();
-    headerPosition = lastScrollY - header.offset().top;
-    containerTop = container.offset().top;
+  window.addEventListener('scroll', () => {
+    lastScrollY = window.scrollY;
 
-    if (lastScrollY < containerTop) {
-      header.css('backgroundPositionY', lastScrollY / 2 + 'px');
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        const containerTop = container.offsetTop;
+
+        if (lastScrollY < containerTop) {
+          header.style.backgroundPosition = `center ${lastScrollY / 2}px`;
+        }
+
+        ticking = false;
+      });
+
+      ticking = true;
     }
-
-    if (lastScrollY > containerTop) {
-      nav.addClass('sticky');
-    }
-
-    if (lastScrollY === 0) {
-      nav.removeClass('sticky');
-    }
-  });
-});
-
-const contactForm = $('#contact-form');
-
-contactForm.submit((e) => {
-  e.preventDefault();
-  const submitButton = $('.send');
-  const defaultSubmitText = submitButton.val();
-
-  $.post({
-    url: '/',
-    contentType: 'application/x-www-form-urlencoded',
-    data: $(contactForm).serialize(),
-    beforeSend: () => {
-      submitButton.prop('disabled', true).val('Sending message…');
-    },
-    success: () => {
-      contactForm.append(
-        `<div class='success'>Thank you. Your message has been sent successfully.</div>`
-      );
-      submitButton.val('Message sent!');
-      $('#contact-form')[0].reset();
-      setTimeout(() => {
-        $('.success').fadeOut(function () {
-          $(this).remove();
-        });
-        submitButton.prop('disabled', false).val(defaultSubmitText);
-      }, 5000);
-    },
-    error: () => {
-      contactForm.append(
-        `<div class='error'>Sorry, your message could not be sent. Please email contact@ochremusic.com directly instead.</div>`
-      );
-      submitButton.val('Sending failed!');
-      setTimeout(() => {
-        $('.error').fadeOut(function () {
-          $(this).remove();
-        });
-        submitButton.prop('disabled', false).val(defaultSubmitText);
-      }, 5000);
-    },
   });
 });
